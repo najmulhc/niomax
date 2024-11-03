c73bd343b86a7a0ba9908dc20c666d9c43c52a22
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2024-11-03 17:29:16 +0600

    component structure of the app is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import About from "./components/about/About";
+import BookingForm from "./components/booking-form/BookingForm";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Menu from "./components/menu/Menu";
+import Testimonials from "./components/testimonials/Testimonials";
+
+// this is the component to wrap everything in the page
 const App = () => {
   return (
-    <main>
-      <h1 className="text-red-500">Lroem ipsum dolo</h1>
-    </main>
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Menu />
+      <BookingForm />
+      <Testimonials />
+      <Footer />
+    </>
   );
 };
 
