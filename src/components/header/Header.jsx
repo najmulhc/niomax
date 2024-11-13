f0f2e69c624128cd8280748b479de293569dd9ad
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2024-11-13 22:05:04 +0600

    header basic part is ready

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import logo from "../../assets/logo.svg";
+import Button from "../shared/Button";
 
 const Header = () => {
+  const pages = ["Home", "About", "Portfolio", "Clients", "Blog", "Contact"];
   return (
-    <div>Header</div>
-  )
-}
+    <header className="sticky z-40 bg-red w-full md:h-[6.75rem] h-[4.813rem] top-0 left-0 py-8 px-[1.875rem]  ">
+      <div className="md:w-[82.5rem] h-full flex justify-between items-center mx-auto">
+        <div className="flex justify-start items-center gap-12">
+          <img src={logo} alt="" />
 
-export default Header
+          <div className="flex justify-start items-center text-white gap-10 font-roboto">
+            {pages.map((item) => (
+              <a href="#" className="hover:text-yellow" key={item}>
+                {item}
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <Button>Book a table</Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
